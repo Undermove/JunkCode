88616554259b5d6fd79933d2244e4e93f0c1a76e
Afonchenko Dmitry <undermove@yandex.ru>
2017-10-27 14:11:40 +0300

    Add DateTimeProfiling

---
+++ DateTimePerformanceMeasure/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimePerformanceMeasure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int iterationsCount = 1000000;
+
+            long temp = 0;
+            DateTime tempDate;
+
+            Stopwatch sw = new Stopwatch();
+
+            for (int j = 0; j < 4; j++)
+            {
+                decimal dateTimeCallTicks = 0;
+                for (int i = 0; i < iterationsCount; i++)
+                {
+                    sw.Restart();
+                    tempDate = DateTime.UtcNow;
+                    sw.Stop();
+                    dateTimeCallTicks += sw.ElapsedTicks;
+                }
+                decimal median = dateTimeCallTicks / iterationsCount;
+                Console.WriteLine($"DateTime {median}");
+
+                decimal tickCountCallTicks = 0;
+                for (int i = 0; i < iterationsCount; i++)
+                {
+                    sw.Restart();
+                    temp = Environment.TickCount;
+                    sw.Stop();
+                    tickCountCallTicks += sw.ElapsedTicks;
+                }
+                median = tickCountCallTicks / iterationsCount;
+                Console.WriteLine($"TickCount {median}");
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
