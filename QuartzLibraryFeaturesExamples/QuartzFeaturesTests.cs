43a97a5fbc10b13d88c763e12e5f52b43b2c7fa6
Undermove <undermove@yandex.ru>
2018-08-24 16:51:58 +0300

    Some quartz tests added

--- QuartzLibraryFeaturesExamples/QuartzFeaturesTests.cs
+++ QuartzLibraryFeaturesExamples/QuartzFeaturesTests.cs
@@ -15,38 +15,50 @@ namespace QuartzLibraryFeaturesExamples
 	        IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler().Result;
 
 	        scheduler.Start();
-	        TriggerObject triggerObject = new TriggerObject();
+	        TriggerObject triggerObject = new TriggerObject(){Id = 1};
+	        TriggerObject triggerObject2 = new TriggerObject(){Id = 2};
 
 	        var id = Guid.NewGuid().ToString();
 
-	        IJobDetail job = JobBuilder.Create<HelloJob>()
+			IJobDetail job = JobBuilder.Create<HelloJob>()
+				.WithIdentity(id, "group1")
+				.UsingJobData(new JobDataMap()
+				{
+					{"triggerObject", triggerObject },
+				})
+				.Build();
+			IJobDetail job2 = JobBuilder.Create<HelloJob>()
 		        .WithIdentity(id, "group1")
 		        .UsingJobData(new JobDataMap()
 		        {
-			        {"triggerObject", triggerObject },
+			        {"triggerObject", triggerObject2 },
 		        })
-				.Build();
+		        .Build();
 
 			TimeSpan time = DateTime.Now.TimeOfDay.Add(TimeSpan.FromSeconds(3));
 
-	        ITrigger trigger = TriggerBuilder.Create()
-		        .WithIdentity(id, "group1")
-		        .StartNow()
+			ITrigger trigger = TriggerBuilder.Create()
+				.WithIdentity(id, "group1")
+				.StartNow()
 				.WithCronSchedule($"{time.Seconds} {time.Minutes} {time.Hours} * * ?")
 				.Build();
 
 			scheduler.ScheduleJob(job, trigger);
+			scheduler.DeleteJob(job.Key);
+			scheduler.ScheduleJob(job2, trigger);
 
-	        Thread.Sleep(TimeSpan.FromSeconds(10));
+			Thread.Sleep(TimeSpan.FromSeconds(10));
 
 	        scheduler.Shutdown();
 
-			Assert.True(triggerObject.IsTriggered);
+			Assert.False(triggerObject.IsTriggered);
+			Assert.True(triggerObject2.IsTriggered);
 		}
     }
 
 	public class TriggerObject
 	{
+		public int Id { get; set; }
 		public bool IsTriggered { get; set; }
 	}
 
