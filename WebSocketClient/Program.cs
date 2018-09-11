9b192ac62a87496b978b68652445e1e21938732c
Undermove <undermove@yandex.ru>
2018-09-11 10:57:48 +0300

    Some examples with TCP or websoket or selenium

---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace WebSocketClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var ws = new WebSocket("ws://localhost:9999"))
+			{
+				ws.OnMessage += (sender, e) =>
+					Console.WriteLine("Laputa says: " + e.Data);
+
+				ws.Connect();
+				ws.Send("BALUS");
+				Console.ReadKey(true);
+			}
+		}
+	}
+}
