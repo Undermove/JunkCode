e90fbf439114c60b21063d65970c0735de7cd3c0
Dmitry Afonchenko <undermove@yandex.ru>
2020-10-06 22:14:56 +0300

    Add linked list memory leaker

---
+++ LinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LinkedListTest;
+
+namespace LinkedList {
+    class Program {
+        static void Main (string[] args) {
+            LinkedListTest.LinkedList<int> linkedList = new LinkedListTest.LinkedList<int> ();
+
+            Int64 iterationsCount = 0;
+            try {
+                List<Task> tasks = new List<Task> ();
+                for (int i = 0; i < 4; i++) {
+                    Thread thread = Thread.
+                    tasks.Add (
+                        Task.Factory.StartNew (() => {
+                            while (true) {
+                                linkedList.AddCellToEnd (new LinkedListCell<int> (6));
+                                iterationsCount++;
+                                Console.WriteLine ($"Iteration: {iterationsCount}");
+                            }
+                        })
+                    );
+                }
+                Console.ReadLine();
+            } catch (OutOfMemoryException) {
+                Console.WriteLine ($"IterationsCount is {iterationsCount}");
+                throw;
+            }
+        }
+    }
+}
